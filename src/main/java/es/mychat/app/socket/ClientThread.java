2bd5430c6ffa4c467db841a343344d1b6c3a1e93
Enrique Sanchez <esanchez.devel@gmail.com>
2023-02-02 19:50:13 +0100

    feat: manage the connection closed by the client to keep the server alive

--- src/main/java/es/mychat/app/socket/ClientThread.java
+++ src/main/java/es/mychat/app/socket/ClientThread.java
@@ -1,6 +1,7 @@
 package es.mychat.app.socket;
 
 import java.io.DataInputStream;
+import java.io.IOException;
 import java.net.Socket;
 
 import es.mychat.app.model.entity.ClientEntity;
@@ -24,10 +25,21 @@ public class ClientThread extends Thread {
 				
 				String messageReceived = input.readUTF(); //message received from the client
 				
-				System.out.println(messageReceived);
-				
-				for (ClientEntity client : SocketServer.CLIENTS_CONNECTED) {
-					client.getOutput().writeUTF(getNickFromMessage(messageReceived) + ">> " + getContentFromMessage(messageReceived));
+				if ("exit".equals(getContentFromMessage(messageReceived))) {
+					SocketServer.CLIENTS_CONNECTED.removeIf(client -> client.getId().equals(getNickFromMessage(messageReceived)));
+					SocketServer.CLIENTS_CONNECTED.forEach(client -> {
+						try {
+							client.getOutput().writeUTF("<------" + getNickFromMessage(messageReceived) + " left the session------>");
+						} catch (IOException e) {
+							System.out.println("Error sending bye message to all clients");
+						}
+					});
+				} else {
+					System.out.println(messageReceived);
+					
+					for (ClientEntity client : SocketServer.CLIENTS_CONNECTED) {
+						client.getOutput().writeUTF(getNickFromMessage(messageReceived) + ">> " + getContentFromMessage(messageReceived));
+					}
 				}
 			}
 		} catch (Exception e) {
